f0bdc78b08dd8012321a17d34138b19c409c0231
Thomas Jang <tom@axisj.com>
2017-07-19 00:51:34 +0900

    module export 구문 수정

--- dist/ES5/index.js
+++ dist/ES5/index.js
@@ -3,18 +3,23 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.AX6UIMask = exports.AX6UIFormatter_formatter = exports.AX6UIFormatter = exports.AX6Mustache = exports.AX6UICore = exports.AX6Util = exports.AX6Info = undefined;
 
 var _AX6Info = require("./AX6Info");
 
 var _AX6Info2 = _interopRequireDefault(_AX6Info);
 
-var _AX6UICore = require("./AX6UICore");
+var _AX6Util = require("./AX6Util");
 
-var _AX6UICore2 = _interopRequireDefault(_AX6UICore);
+var _AX6Util2 = _interopRequireDefault(_AX6Util);
 
 var _AX6Mustache = require("./AX6Mustache");
 
 var _AX6Mustache2 = _interopRequireDefault(_AX6Mustache);
+
+var _AX6UICore = require("./AX6UICore");
+
+var _AX6UICore2 = _interopRequireDefault(_AX6UICore);
 
 var _AX6UIFormatter = require("./AX6UIFormatter");
 
@@ -30,8 +35,16 @@ var _AX6UIMask2 = _interopRequireDefault(_AX6UIMask);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+exports.AX6Info = _AX6Info2.default;
+exports.AX6Util = _AX6Util2.default;
+exports.AX6UICore = _AX6UICore2.default;
+exports.AX6Mustache = _AX6Mustache2.default;
+exports.AX6UIFormatter = _AX6UIFormatter2.default;
+exports.AX6UIFormatter_formatter = _AX6UIFormatterFormatter2.default;
+exports.AX6UIMask = _AX6UIMask2.default;
 exports.default = {
     AX6Info: _AX6Info2.default,
+    AX6Util: _AX6Util2.default,
     AX6UICore: _AX6UICore2.default,
     AX6Mustache: _AX6Mustache2.default,
     AX6UIFormatter: _AX6UIFormatter2.default,
--- dist/ES6/index.js
+++ dist/ES6/index.js
@@ -1,15 +1,19 @@
 import AX6Info from "./AX6Info";
-import AX6UICore from "./AX6UICore";
+import AX6Util from "./AX6Util";
 import AX6Mustache from "./AX6Mustache";
+import AX6UICore from "./AX6UICore";
 import AX6UIFormatter from "./AX6UIFormatter";
 import AX6UIFormatter_formatter from "./AX6UIFormatter-formatter";
 import AX6UIMask from "./AX6UIMask";
 
+export { AX6Info, AX6Util, AX6UICore, AX6Mustache, AX6UIFormatter, AX6UIFormatter_formatter, AX6UIMask };
+
 export default {
-    AX6Info: AX6Info,
-    AX6UICore: AX6UICore,
-    AX6Mustache: AX6Mustache,
-    AX6UIFormatter: AX6UIFormatter,
-    AX6UIFormatter_formatter: AX6UIFormatter_formatter,
-    AX6UIMask: AX6UIMask
+    AX6Info,
+    AX6Util,
+    AX6UICore,
+    AX6Mustache,
+    AX6UIFormatter,
+    AX6UIFormatter_formatter,
+    AX6UIMask
 };

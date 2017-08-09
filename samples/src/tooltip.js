ab9ea8b59075df5dbfbdb526e85ab7fb4564bc75
Thomas Jang <tom@axisj.com>
2017-08-09 13:57:29 +0900

    AX6UITooltip 완료

--- samples/src/tooltip.js
+++ samples/src/tooltip.js
@@ -5,11 +5,11 @@ const $body = $("#sample-body");
 let el = `
 <br/>
 <br/>
-<a href="#" data-ax6ui-tooltip="tooltip">default</a>
-<a href="#" data-ax6ui-tooltip="tooltip" class="tooltip-bottom">bottom</a>
-<a href="#" data-ax6ui-tooltip="tooltip" class="tooltip-top">top</a>
-<a href="#" data-ax6ui-tooltip="tooltip" class="tooltip-left">left</a>
-<a href="#" data-ax6ui-tooltip="tooltip" class="tooltip-bottom">right</a>
+<button data-ax6ui-tooltip="tooltip default" class="btn">default</button>
+<button data-ax6ui-tooltip="tooltip bottom" class="btn tooltip-bottom">bottom</button>
+<button data-ax6ui-tooltip="tooltip top" class="btn tooltip-top">top</button>
+<button data-ax6ui-tooltip="tooltip left" class="btn tooltip-left">left</button>
+<button data-ax6ui-tooltip="tooltip right" class="btn tooltip-right">right</button>
 `;
 $body.append(el);
 
